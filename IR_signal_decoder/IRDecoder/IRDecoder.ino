8dc0cac458b60c0715dbe7ba2faa675b77558a58
nir6898 <nirvegh98@gmail.com>
2024-08-24 12:53:58 +0300

    start of transmitting

--- IR_signal_decoder/IRDecoder/IRDecoder.ino
+++ IR_signal_decoder/IRDecoder/IRDecoder.ino
@@ -15,7 +15,21 @@ void loop()
 {
   if (IrReceiver.decode())
     {
+      Serial.println(":::START:::");
+      Serial.println(":::Decoded Data:::");
+      Serial.println(IrReceiver.decodedIRData.protocol);
+      Serial.println(IrReceiver.decodedIRData.address);
+      Serial.println(IrReceiver.decodedIRData.command);
+      Serial.println(IrReceiver.decodedIRData.extra);
+      Serial.println(IrReceiver.decodedIRData.numberOfBits);
+      Serial.println(IrReceiver.decodedIRData.flags);
       Serial.println(IrReceiver.decodedIRData.decodedRawData, 2);
+      Serial.println(":::Result Raw:::");
+      IrReceiver.printIRResultRawFormatted(&Serial, true);
+      Serial.println(":::Send Usage:::");
+      IrReceiver.printIRSendUsage(&Serial);
+      Serial.println(":::END:::");
+      // Serial.println(IrReceiver.decodedIRData.decodedRawDataArray);
       IrReceiver.resume();
     }
   delay(500);
--- IR_transmitter/IR_transmitter/IR_transmitter.ino
+++ IR_transmitter/IR_transmitter/IR_transmitter.ino
@@ -1,9 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <IRremote.h>
 
-}
+#define IR_SEND_PIN         4
+#define IR_RECIVE_PIN       2
+
+#define IR_DATA_BUFFER_LEN  500
+
+uint16_t rawDataBuffer[IR_DATA_BUFFER_LEN];
+int rawDataBuffer_len = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial)
+    ;
+
+  IrSender.begin(IR_SEND_PIN); // Start with IR_SEND_PIN -which is defined in PinDefinitionsAndMore.h- as send pin and enable feedback LED at default feedback LED pin
+  IrReceiver.begin(IR_RECIVE_PIN, ENABLE_LED_FEEDBACK);
+
+  Serial.println("waiting for command to be recorded...");
+  while(!(IrReceiver.decode()))
+    ;
+
+  Serial.println("Got command recorded!");
+  
+  for (int i = 0; (i < IrReceiver.decodedIRData.rawlen) && (i < IR_DATA_BUFFER_LEN); i++) {
+    rawDataBuffer[i] = IrReceiver.decodedIRData.rawDataPtr->rawbuf[i] * MICROS_PER_TICK;
+    rawDataBuffer_len = i;
+    Serial.print(rawDataBuffer[i]);
+    Serial.print(", ");
+  }
+  // Serial.println("commans length: ");
+  // Serial.println(sizeof(rawData) / sizeof(rawData[0]));
+
+
+} 
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("Sending command ->");
+  Serial.println("commans length: ");
+  Serial.print(rawDataBuffer_len);
+  IrSender.sendRaw(rawDataBuffer, rawDataBuffer_len, 38);
+  delay(5000);
 }
